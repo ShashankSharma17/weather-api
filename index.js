4bcef459ad1856766c83d2773e8a668d330fd8c5
ShashankSharma17 <38305189+ShashankSharma17@users.noreply.github.com>
2019-01-27 20:46:24 +0530

    basic functionality complete

---
+++ index.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const argv = require('yargs').argv;
+const config = require('./config')
+
+var apiKey = config 
+let city = argv.c || 'delhi'
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${apiKey}`
+
+
+request(url, function (err, response, body) {
+	if(err) {
+		console.log('error:', error)
+	} else {
+		let weather = JSON.parse(body)
+		let message = `It's ${weather.main.temp} degrees in ${weather.name}!`
+		console.log(message)
+	}
+});
